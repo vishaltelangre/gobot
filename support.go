37cb8fe1d3857efd666fe655c5b49e4d22f53dc0
Vishal Telangre <the@vishaltelangre.com>
2014-05-13 00:11:22 +0530

    fix: pass slice splats as argument while executing system command

--- support.go
+++ support.go
@@ -23,12 +23,12 @@ func containsString(slice []string, item string) bool {
 // result of the command from stdio to the interactive session.
 func execCmdAndPrintResult(cmdWithArg string) {
 	var cmdFields = strings.Fields(cmdWithArg)
-	cmd := exec.Command(cmdFields[0], cmdFields[1])
+	cmd := exec.Command(cmdFields[0], cmdFields[1:]...)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println("Don't mind, something's broken!")
+		fmt.Println("Don't mind, something's broken!", "\n", err)
 	}
 	fmt.Println(strings.TrimSpace(out.String()))
 }
